f2d22aec5c244960ee9a7fc5480ba863c28dbb08
CoelhoLado <naac.1999@gmail.com>
2019-12-27 15:28:20 +0000

    login e registo adicionados ao cliente

--- src/ClientServer/Cliente.java
+++ src/ClientServer/Cliente.java
@@ -1,6 +1,7 @@
 package ClientServer;
 
 import Exceptions.DadosInexistentesException;
+import Exceptions.DadosJaExistemException;
 import MediaCenter.MediaCenterRemoto;
 import java.util.Scanner;
 
@@ -11,20 +12,31 @@ public class Cliente {
         this.mcr = new MediaCenterRemoto();
     }
 
-    public void clientStart(){
+    public void clientStart() {
         Scanner scanner = new Scanner(System.in);
         String info, nome, pass;
-        info = scanner.nextLine();
-        switch (info){
-            case "login":
-                nome = scanner.nextLine();
-                pass = scanner.nextLine();
-                try {
-                    mcr.login(nome, pass);
-                } catch (DadosInexistentesException e){
-                    System.out.println("Conta Invalida");
-                }
-                break;
+
+        while ((info = scanner.nextLine()) != null) {
+            switch (info) {
+                case "login":
+                    nome = scanner.nextLine();
+                    pass = scanner.nextLine();
+                    try {
+                        mcr.login(nome, pass);
+                    } catch (DadosInexistentesException e) {
+                        System.out.println("Conta Invalida");
+                    }
+                    break;
+                case "registo":
+                    nome = scanner.nextLine();
+                    pass = scanner.nextLine();
+                    try {
+                        mcr.registo(nome, pass);
+                    } catch (DadosJaExistemException e) {
+                        System.out.println("Conta Invalida");
+                    }
+
+            }
         }
     }
 }
